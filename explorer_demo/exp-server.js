923d364f1b196ac2658cdfd85f76dbb55ff1ab70
mahesh gutala <mahesh gutala>
2016-11-17 15:55:57 -0500

    Added Explorer Demo

---
+++ explorer_demo/exp-server.js
@@ -0,0 +1,373 @@
+/*Copyright DTCC 2016 All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
+//"use strict";
+
+// Optional. You will see this name in eg. 'ps' or 'top' command
+process.title = process.env.PROC_NAME || 'HyperlegerExplorer';
+
+// Port where we'll run the websocket server
+var webSocketsServerPort = process.env.HTTP_PORT || 9090;
+
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname+'/webcontent/static/scripts'));
+app.use(express.static(__dirname+'/webcontent/static/css'));
+app.use(express.static(__dirname+'/webcontent/static/images'));
+app.use(express.static(__dirname+'/webcontent/static/scripts/socket.io-client'));
+app.use(express.static(__dirname+'/webcontent/static/scripts/angular'));
+app.use(express.static(__dirname+'/webcontent/static/scripts/angular-animate'));
+app.use(express.static(__dirname+'/webcontent/static/jquery-ui-1.12.1.custom'));
+
+
+var bodyParser = require('body-parser');
+/** bodyParser.urlencoded(options)
+ * Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST)
+ * and exposes the resulting object (containing the keys and values) on req.body
+ */
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+/**bodyParser.json(options)
+ * Parses the text as JSON and exposes the resulting object on req.body.
+ */
+app.use(bodyParser.json());
+
+
+var peerIntf = require('./hyperledgerpeerintf');
+
+
+
+//var dots = require("dot").process({ path: "./views"});
+require("dot").process({
+	templateSettings : {
+	  evaluate:    /\(\(([\s\S]+?)\)\)/g,
+	  interpolate: /\(\(=([\s\S]+?)\)\)/g,
+	  encode:      /\(\(!([\s\S]+?)\)\)/g,
+	  use:         /\(\(#([\s\S]+?)\)\)/g,
+	  define:      /\(\(##\s*([\w\.$]+)\s*(\:|=)([\s\S]+?)#\)\)/g,
+	  conditional: /\(\(\?(\?)?\s*([\s\S]*?)\s*\)\)/g,
+	  iterate:     /\(\(~\s*(?:\)\)|([\s\S]+?)\s*\:\s*([\w$]+)\s*(?:\:\s*([\w$]+))?\s*\)\))/g,
+	  varname: 'it',
+	  strip: true,
+	  append: true,
+	  selfcontained: false
+	}
+	,global: "_page.render"
+	, destination: __dirname + "/webcontent/dynamic/"
+	, path: (__dirname + "/templates")
+});
+
+var render = require( __dirname + "/webcontent/dynamic/");
+
+//Hyperledger UI
+var ledgerData = { "chain" : { "height" : 0} , "peers" : {} , "blocks" : [], "chainCodes" : []};
+var statsData = null;
+app.get("/", function(req, res) {
+
+	try {
+		var ledgerDataStr = JSON.stringify(ledgerData);
+		var statsDataStr = JSON.stringify(statsData);
+		res.send(render.HyperlegerExplorer({'ledgerData' : ledgerDataStr , 'statsData':statsDataStr}));
+	} catch(e) {
+		console.log(" Error retrieving initial hyperledger info "+e);
+		return res.send(render.HyperlegerExplorer(0));
+	}
+
+
+});
+
+app.get('/block/:blockNum', function(req, res) {
+	try {
+		var blockNum =  req.params.blockNum.substring(1);
+		console.log('block Num requested ',blockNum);
+		peerIntf.block(blockNum, function (obj) {
+			res.send(obj);
+		});
+	} catch(e) {
+		res.send({});
+	}
+});
+
+app.get('/chainCode/src/:id', function(req, res) {
+	try {
+		var id =  req.params.id.substring(1);
+		console.log('chaincode id requested ',id);
+		peerIntf.chainCodeSrc(id, function (r) {
+				if(r.error) {
+					console.log("Error in reading chainCode ",e)
+					res.writeHead(200, {
+			        'Content-Type': 'text/plain',
+			        'Content-Length': 0
+			    });
+
+				} else {
+							res.writeHead(200, {
+				        'Content-Type': 'text/plain',
+				        'Content-Length': r.size
+				    });
+
+				    r.readStream.pipe(res);
+					}
+			});
+	} catch(e) {
+		console.log("Exception in reading chainCode ")
+		res.writeHead(200, {
+        'Content-Type': 'text/plain',
+        'Content-Length': 0
+    });
+	}
+});
+
+app.post('/chainCode/invoke/:req', function(req, res) {
+	try {
+		console.log(req.params);
+		var args = req.params.req.split(':');
+		var id =  args[0]
+		var oper =  args[1];
+		console.log('chaincode id requested ',id);
+		console.log('Oper requested ',oper);
+		console.log('request ',req.body );
+		if(oper == 'Deploy' ) {
+			peerIntf.chainCodeDeploy(id, JSON.stringify(req.body) , function (r) {
+				if(r.error) {
+					console.log("Error in deploying chainCode ",e)
+					res.send('{"error" : "Failed to deploy chaincode."}');
+				} else {
+					res.send('{"success" : "Successfully deployed chaincode."}');		
+				}
+			});
+		} else if(oper == 'Invoke' ) {
+			peerIntf.chainCodeInvoke(id, JSON.stringify(req.body) , function (r) {
+				if(r.error) {
+					console.log("Error in invoking chainCode ",e)
+					res.send('{"error" : "Failed to incoke chaincode."}');
+				} else {
+					res.send('{"success" : "Successfully invoked chaincode."}');		
+				}
+			});
+		} else if(oper == 'Query' ) {
+			peerIntf.chainCodeQuery(id, JSON.stringify(req.body) , function (r) {
+				if(r.error) {
+					console.log("Error in querying chainCode ",e)
+					res.send('{"error" : "Failed to query chaincode."}');
+				} else {
+					res.send('{"success" : "Successfully queried chaincode." , "data" : ' +JSON.stringify(r.data)+'}');		
+				}
+			});
+		}
+	} catch(e) {
+		console.log("Exception in invoking chainCode " + e)
+		res.send('{"error" : "Exception in processing"}');
+	}
+});
+
+var locked = false;
+var initial = true;
+var getLedgerInfo = function(callBk) {
+	if(locked) {
+		console.log('Waiting to retrieve ledger data ...');
+		setTimeout(function(){ getLedgerInfo(callBk);}, 1000);
+		return;
+	}
+	try {
+		var currHeight = ledgerData.chain.height;
+
+		peerIntf.chain(
+			function(obj) {
+				ledgerData.chain = obj;
+				peerIntf.peers(
+					function(obj) {
+						ledgerData.peers = obj;
+						//console.log('height : '+ledgerData.chain.height);
+						if(initial) {
+							var start = 0;
+							if (ledgerData.chain.height > 100)
+								start = ledgerData.chain.height - 99;
+							//dont load all the blocks. only the latest 100.
+							for (var i = 0; i < start; i++)
+								ledgerData.blocks.push(null);
+							currHeight = start;
+							initial = false;
+						}
+						//console.log('loading ',currHeight);
+						var blockFunc = function(obj) {
+							ledgerData.blocks.push(obj);
+							if(ledgerData.blocks.length > 100) {
+								ledgerData.blocks[ledgerData.blocks.length - 101] = null;
+							}
+							currHeight = ledgerData.blocks.length;
+							if(currHeight == ledgerData.chain.height) {
+								peerIntf.availableChainCodes( function( list) {
+									//console.log('chaincodes :',list);
+									ledgerData.chainCodes = list;
+									callBk(ledgerData);
+									locked = false;
+								});
+							} else
+								peerIntf.block((currHeight++),blockFunc);
+						}
+						if(currHeight != ledgerData.chain.height)
+							peerIntf.block(currHeight,blockFunc);
+
+					}
+				)
+			}
+		);
+	} catch(e) {
+		console.log(e);
+		locked = false;
+		throw e;
+	}
+}
+
+var newBlockArrived = true; //initial value is true
+
+//initial load
+getLedgerInfo( function () {
+	try {
+		//console.log('Ledger data retrieved.');
+		//start listener and Web sockets for updates
+		var server = require('http').createServer(app);
+		var io = require('socket.io')(server);
+		setInterval(
+			function() {
+				var prevPeers = ledgerData.peers;
+				var prevHeight = ledgerData.chain.height;
+				var newData = {};
+				getLedgerInfo( function() {
+					if(JSON.stringify(prevPeers) != JSON.stringify(ledgerData.peers))
+						newData.peers = ledgerData.peers;
+					if(prevHeight != ledgerData.chain.height) {
+						newData.chain = ledgerData.chain;
+						newBlocks = new Array();
+						for(var i = prevHeight; i < ledgerData.chain.height; i++)
+							newBlocks.push(ledgerData.blocks[i]);
+						newData.blocks = newBlocks;
+					}
+					newData.chainCodes = ledgerData.chainCodes;
+					if(newData.peers || newData.blocks) {
+						io.emit('update', JSON.stringify(newData));
+						newBlockArrived= true;
+					}
+				});
+			}
+			, 2000);
+		setInterval(
+			function() {
+				if(!newBlockArrived)
+					return;
+				newBlockArrived = false;
+				var endSecs = -1;
+				var txnRate = 0;
+				var blkRate = 0;
+				var txnCount = 0;
+				var txnLatency = 0;
+				var currTime;
+				var blkTxGraph = {
+					block : [],
+					txs: []
+				}
+				var txRateGraph = {"time":[],"txRate":[]}
+				var blkRateGraph = {"time":[],"blkRate":[]}
+				statsData = {
+					"checkTime" : "",
+					"avgTxnLatency" : "",
+					"txnRate": "",
+					"mineRate": "",
+					"txRateGraph":txRateGraph,
+					"blkRateGraph":blkRateGraph
+				};
+
+				for (var i = ledgerData.chain.height - 1; i > 0; i--) {
+					if(txRateGraph.time.length == 20)
+						break;
+					var block = ledgerData.blocks[i];
+					if (!block || !block.nonHashData)
+						continue;
+
+					if (blkTxGraph.block.length < 20) {
+						blkTxGraph.block.push(i);
+						if(block.transactions && block.transactions.length > 0)
+							blkTxGraph.txs.push(block.transactions.length);
+					}
+					if (endSecs < 0) {
+						endSecs = block.nonHashData.localLedgerCommitTimestamp.seconds;
+						currTime = new Date(null);
+						currTime.setSeconds(endSecs);
+						currTime = currTime.toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
+					}
+					if (block.nonHashData.localLedgerCommitTimestamp.seconds >= (endSecs - 10)) {
+						blkRate++;
+						if(block.transactions && block.transactions.length > 0)
+						for (var k = 0; k < block.transactions.length; k++) {
+							txnRate++;
+							txnCount++;
+							txnLatency += (block.nonHashData.localLedgerCommitTimestamp.seconds - block.transactions[k].timestamp.seconds);
+						}
+					} else {
+						//console.log("new row " , txRateGraph.time.length);
+						txnRate = Math.round(txnRate/10);
+						blkRate = Math.round(blkRate/10);
+
+						statsData = {
+							"checkTime" : currTime,
+							"avgTxnLatency" : Math.round((txnLatency/txnCount)*1000),
+							"txnRate": txnRate,
+							"mineRate": blkRate
+						}
+
+
+						txRateGraph.time. push( currTime);
+						txRateGraph.txRate. push( txnRate);
+
+						blkRateGraph.time. push( currTime);
+						blkRateGraph.blkRate. push( blkRate);
+
+						endSecs = -1;
+						txnRate = 0;
+						blkRate = 0;
+						txnCount = 0;
+						txnLatency = 0;
+					}
+
+				}
+
+				statsData = {
+					"checkTime" : statsData.checkTime,
+					"avgTxnLatency" : statsData.avgTxnLatency,
+					"txnRate": statsData.txnRate,
+					"mineRate": statsData.mineRate,
+					"txRateGraph":txRateGraph,
+					"blkRateGraph":blkRateGraph
+				}
+
+				statsData.txRateGraph = { "time" : txRateGraph.time.reverse() , "txRate" :txRateGraph.txRate.reverse() };
+				statsData.blkRateGraph = { "time" : blkRateGraph.time.reverse() , "blkRate" : blkRateGraph.blkRate.reverse() };
+				statsData.blkTxGraph = { "block" : blkTxGraph.block.reverse() , "txs" : blkTxGraph.txs.reverse() };
+				//simulated values for now. Will be fixed soon.
+				var x = Math.floor(Math.random() * 95) + 76,y=Math.floor(Math.random() * 106) + 91,z=Math.floor(Math.random() * 64) + 37
+				statsData.chTx = { "chainCodes" : ["ch01","ch02","ch03"] , "counts": [x,y,z] };
+				var x= Math.floor(Math.random() * 834) + 631,y=Math.floor(Math.random() * 232) + 46,z=Math.floor(Math.random() * 56) + 32
+				statsData.apprTx = { "stats" : ["Approved","Pending","Rejected"] , "counts": [x,y,z] };
+
+				console.log(' statsData ',statsData);
+				io.emit('stats',JSON.stringify(statsData));
+			}
+			, 5000);
+		server.listen(webSocketsServerPort);
+	} catch(e) {
+		console.log('Ledger data initialization failed.');
+		exit(-1);
+	}
+});
---
+++ explorer_demo/hyperledgerpeerintf/hyperledgerpeerintf.js
@@ -0,0 +1,269 @@
+/*Copyright DTCC 2016 All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
+
+var grep = require('simple-grep');
+var fs = require('fs');
+
+var HyperledgerPeerIntf = function() {
+	const hyperLedgerRESTEndpoint = process.env.HYP_REST_ENDPOINT || "http://127.0.0.1:7050";
+	var async = require('async');
+	var request = require('request');
+
+	this.chainCodes = null;
+
+	this.restCall = function(uri,completion) {
+		var obj;
+		async.series( [function (callback) {
+				//console.log( ' Querying Hyperledger ' ,uri);
+				request(hyperLedgerRESTEndpoint+uri, function (error, response, body) {
+					if (!error && response.statusCode == 200) {
+						//console.log( ' resp ' , body);
+						if(body == null)
+							callback(null,null);
+						else {
+							obj = JSON.parse(body)
+							callback(null,obj);
+						}
+					} else {
+						console.log(error);
+						callback(error);
+						//throw error;
+					}
+
+				})
+			},
+			function(callback) {
+				   completion(obj);
+				   callback();
+			 }
+			]
+		);
+
+	}
+}
+
+
+HyperledgerPeerIntf.prototype.chain = function(callBk) {
+	this.restCall('/chain',callBk);
+}
+
+HyperledgerPeerIntf.prototype.peers = function(callBk) {
+	this.restCall('/network/peers',callBk);
+}
+
+HyperledgerPeerIntf.prototype.block = function(blockNum,callBk) {
+	this.restCall('/chain/blocks/'+blockNum,callBk);
+}
+
+var ROOT_GO_PATH = '/opt/gopath'; ///c/Go for windows
+HyperledgerPeerIntf.prototype.availableChainCodes = function(callBk) {
+  if(!this.chainCodes)
+	{
+		var chainCodes = this.chainCodes = new Array();
+		grep('shim.ChaincodeStubInterface', ROOT_GO_PATH+'/src/github.com/hyperledger/fabric/examples/chaincode/go', function(list1){
+	  		//console.log(list1);
+			grep('extends ChaincodeBase', ROOT_GO_PATH+'/src/github.com/hyperledger/fabric/examples/chaincode/java', function(list2){
+	  			//console.log(list2);
+	  			var idx = 0;
+					for( var c = 0; c < list1.length; c++) {
+						if(list1[c].file.match(/\.go$/))
+							chainCodes.push( { "id": idx++, "name" : list1[c].file.replace(/.*\/(.*)\.go$/,'$1'),"file" : list1[c].file, "lang" : "go" , "status":"U" , "chainId":"","txCount":0});
+					}
+
+					for( var c = 0; c < list2.length; c++) {
+						if(list2[c].file.match(/\.java$/))
+							chainCodes.push( { "id": idx++, "name" : list2[c].file.replace(/.*\/(.*)\.java$/,'$1'),"file" : list2[c].file, "lang" : "java" , "status":"U", "chainId":"", "txCount":0});
+					}
+
+					callBk(chainCodes);
+
+			});
+
+		});
+	} else {
+		callBk(this.chainCodes);
+	}
+}
+
+HyperledgerPeerIntf.prototype.chainCodeSrc = function(id,callBk) {
+
+	try {
+		var file = this.chainCodes[id].file//.replace(/\/.*?\/(.*)/,'\/$1')
+		var stat = fs.statSync(file);
+		var readStream = fs.createReadStream(file);
+		callBk({
+			"size" : stat.size,
+			"readStream" : readStream
+		})
+
+	} catch (e) {
+		console.log("Failed to read chaincode" , this.chainCodes[id].file, e);
+		callBk( {
+			"size" : 0,
+			"error" : "Failed to load chaincode source.",
+			"readStream" :null
+		});
+	}
+
+}
+
+var goChaincodePathLen = (process.env.GOPATH+'/src/').length;
+HyperledgerPeerIntf.prototype.chainCodeDeploy = function(id,argsStr,callBk) {
+
+	try {
+		//console.log( " -- "+id+" "+argsStr + " "+this.chainCodes[id].lang + ' chainId '+this.chainCodes[id].chainId)
+		if(this.chainCodes[id].lang == 'go') {
+			var path = this.chainCodes[id].file.replace(/(.*)\/.*\.go/,'$1').substring(goChaincodePathLen);
+			console.log('path '+ path)
+			var cmd = 'peer chaincode deploy -p '+path + ' -c '+argsStr.replace(/.*:\[(.*?),(.*)]/,'{"Function":$1,"Args":[$2]');
+		} else if(this.chainCodes[id].lang == 'java') {
+			var path = this.chainCodes[id].file.replace(/(.*)\/src\/.*/i,'$1');
+			var cmd = 'peer chaincode deploy -l java -p '+path + ' -c '+argsStr;
+		}
+		console.log('Deploying chaincode ['+cmd+']');
+		var c = cmd.split(" ");
+		var cc = this.chainCodes;
+		run_cmd(c[0],c.slice(1,c.length),function(resp) {
+			if(resp.indexOf('\"error\"') > 0) {
+				console.log('Error returned : '+resp);
+				cc[id].chainId = null;
+				cc[id].status = 'U';
+				callBk(JSON.parse(resp));
+			} else {
+				var chainId = resp.split(':')[1].trim();
+				cc[id].chainId = chainId;
+				cc[id].status = 'D';
+				cc[id].txCount = 0;
+				console.log('chainId returned : ['+chainId+']');
+				callBk({ "chainId" : chainId ,"success" : "Chain code deployed successfully."});
+			}
+		});
+		
+
+	} catch (e) {
+		console.log("Failed to deploy chaincode" , this.chainCodes[id].file);
+		callBk( {"error" : "Failed to deploy chaincode."});
+	}
+
+}
+
+HyperledgerPeerIntf.prototype.chainCodeInvoke = function(id,argsStr,callBk) {
+
+	try {
+		console.log( " -- "+id+" "+argsStr + " "+this.chainCodes[id].lang + ' chainId '+this.chainCodes[id].chainId)
+		if(!this.chainCodes[id].chainId) {
+			callBk( '{"error" : "Chaincode not deployed yet."}');
+			return;
+		}
+		if(this.chainCodes[id].lang == 'go') {
+			var path = this.chainCodes[id].file.replace(/(.*)\/.*\.go/,'$1');
+			var cmd = 'peer chaincode invoke -n '+this.chainCodes[id].chainId + ' -c '+argsStr;;
+		} else if(this.chainCodes[id].lang == 'java') {
+			var path = this.chainCodes[id].file.replace(/(.*)\/src\/.*/i,'$1');
+			var cmd = 'peer chaincode invoke -l java -n '+this.chainCodes[id].chainId + ' -c '+argsStr;
+		}
+		console.log('Invoking chaincode ['+cmd+']');
+		var c = cmd.split(" ");
+		var cc = this.chainCodes;
+		run_cmd(c[0],c.slice(1,c.length),function(resp) {
+			if(resp.indexOf('\"error\"') > 0) {
+				console.log('Error returned : '+resp);
+				callBk(JSON.parse(resp));
+			} else {
+				cc[id].txCount++;
+				callBk({ "success" : "Chaincode invoked successfully." });
+			}
+		});
+		
+
+	} catch (e) {
+		console.log("Failed to invoke chaincode" , this.chainCodes[id].file);
+		callBk( '{"error" : "Failed to invoke chaincode."}');
+	}
+
+}
+
+HyperledgerPeerIntf.prototype.chainCodeQuery = function(id,argsStr,callBk) {
+
+	try {
+		console.log( " -- "+id+" "+argsStr + " "+this.chainCodes[id].lang + ' chainId '+this.chainCodes[id].chainId)
+		if(!this.chainCodes[id].chainId) {
+			callBk( '{"error" : "Chaincode not deployed yet."}');
+			return;
+		}
+		if(this.chainCodes[id].lang == 'go') {
+			var path = this.chainCodes[id].file.replace(/(.*)\/.*\.go/,'$1');
+			var cmd = 'peer chaincode query -n '+this.chainCodes[id].chainId + ' -c '+argsStr;
+		} else if(this.chainCodes[id].lang == 'java') {
+			var path = this.chainCodes[id].file.replace(/(.*)\/src\/.*/i,'$1');
+			var cmd = 'peer chaincode query -l java -n '+this.chainCodes[id].chainId + ' -c '+argsStr;
+		}
+		//console.log('Querying chaincode ['+cmd+']');
+		var c = cmd.split(" ");
+		var cc = this.chainCodes;
+		run_cmd(c[0],c.slice(1,c.length),function(resp) {
+			if(resp.indexOf('\"error\"') > 0) {
+				console.log('Error returned : '+resp);
+				callBk(JSON.parse(resp));
+			} else {
+				var data = resp.split(': ')[1].trim();
+				console.log('data returned : ['+data+']');
+				callBk({ "data" : data,"success":"Chaincode query completed successfully."});
+			}
+		});
+		
+
+	} catch (e) {
+		console.log("Failed to query chaincode" , this.chainCodes[id].file);
+		callBk( '{"error" : "Failed to query chaincode."}');
+	}
+
+}
+
+
+/*(function() {
+    var childProcess = require("child_process");
+    var oldSpawn = childProcess.spawn;
+    function mySpawn() {
+        console.log('spawn called');
+        console.log(arguments);
+        var result = oldSpawn.apply(this, arguments);
+        return result;
+    }
+    childProcess.spawn = mySpawn;
+})();*/
+
+function run_cmd(cmd, args, callBack ) {
+	console.log( ' Using Path ',process.env.PATH + " cmd ["+cmd+"] args ["+args+"]");
+    var spawn = require('child_process').spawn;
+    var child = spawn(cmd,args);
+    var resp = "";
+
+    child.stdout.on('data', function (buffer) { 
+		//console.log(" === process ==== ",buffer.toString()) ; 
+		resp += buffer.toString() 
+	});
+	child.stderr.on('data', function (buffer) { 
+		//console.log(" === process err ==== ",buffer.toString()) ; 
+		resp += buffer.toString() 
+	});
+    child.on('close', function(code) { 
+		console.log(" === process end ==== ",code) ; 
+		if(code != 0)
+			callBack( '{ "error" : "Failed to execute peer command" }');
+		else
+			callBack (resp) 
+	});
+}
+
+module.exports = new HyperledgerPeerIntf();
